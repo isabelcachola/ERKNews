5603b1f7b585b4d5acbe83dd179cb064edf19e0b
Isabel Cachola <cachola.isabel@gmail.com>
2017-04-27 14:48:04 -0500

    Removed auth info

--- auth.py
+++ auth.py
@@ -7,10 +7,10 @@ This only contains a class with twitter access info
 class TwitterAuth:
 	# Go to http://dev.twitter.com and create an app. 
 	# The consumer key and secret will be generated for you after
-	consumer_key="oTPo0l2IeUI6RjvxILogY7T0D"
-	consumer_secret="JtqImb6YhZFEqVEyL5ukTQjC1adrdBmOPXPxYMQziasBCB7x9S"
+	consumer_key="XXXXXXXXXXXXXXXXXXXXX"
+	consumer_secret="XXXXXXXXXXXXXXXXXXXXXX"
 
 	# After the step above, you will be redirected to your app's page.
 	# Create an access token under the the "Your access token" section
-	access_token="842969676795858945-aYFZNpFQocERbNT5oSGIdejrpnCyoaP"
-	access_token_secret="Foa471AUQUp2lbc3vYkMKbcZ6Fl9Vi0lFLltNCLIwlIMc"
+	access_token="XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+	access_token_secret="XXXXXXXXXXXXXXXXXXXXXXXXXXX"
